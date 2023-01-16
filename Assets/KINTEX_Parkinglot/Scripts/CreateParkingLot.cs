b27935d7d78f4abd627895f2b6fbb9cdc3f5560a
mambasa <kal990574@gmail.com>
2023-01-16 16:14:05 +0900

    CreateParingLot.cs update

--- Assets/KINTEX_Parkinglot/Scripts/CreateParkingLot.cs
+++ Assets/KINTEX_Parkinglot/Scripts/CreateParkingLot.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class CreateParkingLot : MonoBehaviour
 {
     [SerializeField] private GameObject Lot;
+    [Space(10)]
+    [SerializeField] private List<GameObject> parkingLotAreaA;
+    [SerializeField] private List<GameObject> parkingLotAreaB;
+    [SerializeField] private List<GameObject> parkingLotAreaC;
+    [SerializeField] private List<GameObject> parkingLotAreaD;
+    [SerializeField] private List<GameObject> parkingLotAreaE;
     void Start()
     {
+        var xPosition = 5142.49f;
+        var zPosition = 3582.25f;
+        var yRotation = 79.33f;
         var parkingLot = Instantiate(Lot);
-        parkingLot.transform.position = new Vector3(1, 0, 1);
+        parkingLot.transform.position = new Vector3(xPosition - 8500, 1f, 6000 - zPosition);
+        parkingLot.transform.Rotate(0f, yRotation, 0f);
     }
 
     // Update is called once per frame
